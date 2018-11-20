e699fb3d3747b57c760ecfacef96058eb7c7b654
lishenmiao <lsmiao@goweb.top>
2018-11-20 13:39:12 +0800

    Update ImClient.go

--- ImClient.go
+++ ImClient.go
@@ -26,7 +26,11 @@ func NewImClient(masterSecret,appKey string) *ImClient {
 
 }
 
-func (this * ImClient)SendCustomMsg(msgbody []byte,patch string) string {
+func (this * ImClient) SendCustomMsg(msgbody []byte)  {
+	this.PostBody(msgbody,"/v1/messages")
+}
+
+func (this * ImClient)PostBody(msgbody []byte,patch string) string {
 	req:=httplib.Post(this.BaseUrl+patch)
 	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	req.Header("Authorization",this.AuthCode)
