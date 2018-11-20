d6e94cbf6109221d8b029fd712805b0a1b1b848c
limisao <lism@11td.com>
2018-11-20 16:57:53 +0800

    fat

--- ImClient.go
+++ ImClient.go
@@ -1,14 +1,14 @@
 package jim
 
 import (
-	"encoding/base64"
-	"github.com/astaxie/beego/httplib"
-	"fmt"
 	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"github.com/astaxie/beego/httplib"
 )
 
 const (
-	HOST_NAME_SSL="https://api.im.jpush.cn"
+	HOST_NAME_SSL = "https://api.im.jpush.cn"
 )
 
 type ImClient struct {
@@ -18,30 +18,30 @@ type ImClient struct {
 	BaseUrl      string
 }
 
-func NewImClient(masterSecret,appKey string) *ImClient {
+func NewImClient(masterSecret, appKey string) *ImClient {
 	//base64
-	auth := "Basic " +  base64.StdEncoding.EncodeToString([]byte(appKey+":"+masterSecret))
+	auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(appKey+":"+masterSecret))
 	imer := &ImClient{masterSecret, appKey, auth, HOST_NAME_SSL}
 	return imer
 
 }
 
-func (this * ImClient) SendCustomMsg(msgbody []byte) string  {
-	return this.PostBody(msgbody,"/v1/messages")
+func (this *ImClient) SendCustomMsg(msgbody []byte) string {
+	return this.PostBody(msgbody, "/v1/messages")
 }
 
-func (this * ImClient)PostBody(msgbody []byte,patch string) string {
-	req:=httplib.Post(this.BaseUrl+patch)
+func (this *ImClient) PostBody(msgbody []byte, patch string) string {
+	req := httplib.Post(this.BaseUrl + patch)
 	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
-	req.Header("Authorization",this.AuthCode)
-	req.Header("Content-Type","application/json;charset=utf-8")
+	req.Header("Authorization", this.AuthCode)
+	req.Header("Content-Type", "application/json;charset=utf-8")
 	req.Body(msgbody)
-	 res,err:=req.String()
-	 if err==nil{
-	 	return res
-	 }else {
-	 	fmt.Println("error:",err)
-	 	return "fail"
-	 }
+	res, err := req.String()
+	if err == nil {
+		return res
+	} else {
+		fmt.Println("error:", err)
+		return "fail"
+	}
 
 }
--- Messages.go
+++ Messages.go
@@ -6,62 +6,61 @@ import (
 )
 
 type Custom_MsgType struct {
-	Version int64 `json:"version"`
-	Target_type string `json:"target_type"`
-	Target_id string `json:"target_id"`
-	From_type string `json:"from_type"`
-	From_id string `json:"from_id"`
-	Msg_type string `json:"msg_type"`
-	Create_time int64  `json:"create_time"`
-	From_platform string  `json:"from_platform"`
-	No_offline bool `json:"no_offline"`
-	No_notification bool `json:"no_notification"`
-	Msg_body interface{} `json:"msg_body"`
-
+	Version         int64       `json:"version"`
+	Target_type     string      `json:"target_type"`
+	Target_id       string      `json:"target_id"`
+	From_type       string      `json:"from_type"`
+	From_id         string      `json:"from_id"`
+	Msg_type        string      `json:"msg_type"`
+	Create_time     int64       `json:"create_time"`
+	From_platform   string      `json:"from_platform"`
+	No_offline      bool        `json:"no_offline"`
+	No_notification bool        `json:"no_notification"`
+	Msg_body        interface{} `json:"msg_body"`
 }
 
 func NewCustom_Msg() *Custom_MsgType {
-	msg:=&Custom_MsgType{}
-	return  msg
+	msg := &Custom_MsgType{}
+	return msg
 }
 
-func (this * Custom_MsgType)SetVersion(version int64)  {
-	this.Version=version
-
-}
-
-func (this * Custom_MsgType)SetTarget_type(target_type string)  {
-	this.Target_type=target_type
-
-}
-func (this * Custom_MsgType)SetTarget_id(target_id string)  {
-	this.Target_id=target_id
-
-}
-func (this * Custom_MsgType)SetFrom_type(from_type string)  {
-	this.From_type=from_type
+func (this *Custom_MsgType) SetVersion(version int64) {
+	this.Version = version
 
 }
 
-func (this * Custom_MsgType)SetFrom_id(from_id string)  {
-	this.From_id=from_id
+func (this *Custom_MsgType) SetTarget_type(target_type string) {
+	this.Target_type = target_type
+
+}
+func (this *Custom_MsgType) SetTarget_id(target_id string) {
+	this.Target_id = target_id
+
+}
+func (this *Custom_MsgType) SetFrom_type(from_type string) {
+	this.From_type = from_type
 
 }
 
-func (this * Custom_MsgType)SetMsg_type(msg_type string)  {
-	this.Msg_type=msg_type
+func (this *Custom_MsgType) SetFrom_id(from_id string) {
+	this.From_id = from_id
 
 }
-func (this * Custom_MsgType)SetNo_offline(c bool)  {
-	this.No_offline=c
+
+func (this *Custom_MsgType) SetMsg_type(msg_type string) {
+	this.Msg_type = msg_type
 
 }
-func (this * Custom_MsgType)SetNo_notification(c bool)  {
-	this.No_notification=c
+func (this *Custom_MsgType) SetNo_offline(c bool) {
+	this.No_offline = c
 
 }
-func (this * Custom_MsgType)SetCustomJsonBody(customJson interface{})  {
-	this.Msg_body=customJson
+func (this *Custom_MsgType) SetNo_notification(c bool) {
+	this.No_notification = c
+
+}
+func (this *Custom_MsgType) SetCustomJsonBody(customJson interface{}) {
+	this.Msg_body = customJson
 
 }
 func (this *Custom_MsgType) ToBytes() ([]byte, error) {
